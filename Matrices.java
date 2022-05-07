b136be1cd15469eada1e9f7a75c9cdc43f13f22a
martinrome <martinro@hotmail.com>
2022-05-07 16:51:47 -0500

    Curso Java 21 / Matrices Filas/columnas

---
+++ Matrices.java
@@ -0,0 +1,27 @@
+public class Matrices{
+public static void main(String args[]){
+
+/* Primero va el numero de filas y luego el nuero de columnas */
+
+ int numeros[][] = new int[4][2];
+
+ numeros[0][0] = 15;
+ numeros[0][1] = 16;
+ numeros[1][0] = 17;
+ numeros[1][1] = 18;
+ numeros[2][0] = 19;
+ numeros[2][1] = 20;
+ numeros[3][0] = 21;
+ numeros[3][1] = 22;
+
+ System.out.print(" [" + numeros[0][0] + "] ");
+ System.out.println(" [" + numeros[0][1] + "] ");
+ System.out.print(" [" + numeros[1][0] + "] ");
+ System.out.println(" [" + numeros[1][1] + "] ");
+ System.out.print(" [" + numeros[2][0] + "] ");
+ System.out.println(" [" + numeros[2][1] + "] ");
+ System.out.print(" [" + numeros[3][0] + "] ");
+ System.out.println(" [" + numeros[3][1] + "] ");
+ }
+
+}
