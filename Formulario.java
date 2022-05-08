8ae82d8e238bacc70a98145db73b586dcd15472e
martinrome <martinro@hotmail.com>
2022-05-07 22:14:57 -0500

    Curso Java 25 - Graficos swing y JFrame. Ventana posicion label1

--- Formulario.java
+++ Formulario.java
@@ -1,18 +1,15 @@
 import javax.swing.*;
 
 public class Formulario extends JFrame{
-  private JLabel label1;
 
   public Formulario(){    /* Este es un contructor */
    setLayout(null);
-   label1 = new JLabel("La Geekipedia de Ernesto");
-   label1.setBounds(10,20,200,300);
-   add(label1);
  }
  public static void main(String args[]){
   Formulario formulario1 = new Formulario(); /*clase Formulario que usa JFrame */
-  formulario1.setBounds(0,0,400,300);
+  formulario1.setBounds(0,0,400,550);
   formulario1.setVisible(true);
-  formulario1.setLocationRelativeTo(null); /*Para que aparezca en el centro de la pantalla*/
+  formulario1.setLocationRelativeTo(null); /* Hace que aparesca centrada a pesar de las coordenadas*/
+  formulario1.setResizable(false);    /* Usuario no puede modificar dimensiones de la ventana*/
  }
 }
